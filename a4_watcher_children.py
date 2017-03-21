aa95780ab17e3fdc39b8fda51ea56f9ea8a719a3
rick_cs_chen <rick_cs_chen@wistron.com>
2017-03-21 14:55:19 +0800

    add master select

--- a4_watcher_children.py
+++ a4_watcher_children.py
@@ -3,6 +3,8 @@ from kazoo.client import KazooClient, KazooState, KeeperState
 import logging
 logging.basicConfig(level=logging.DEBUG)
 ROOT_PATH='/schedulers'
+my_id=None
+
 def getZK(hosts):
     return KazooClient(hosts, read_only=True)
 
@@ -24,12 +26,17 @@ def my_rw(state):
 
 def my_watch_chilren(children):
     logging.info('children: %s' %children)
-
+    if min([int(id) for id in children]) == my_id:
+       logging.info('====== ITs MY TERM GOGOGO!!!!!')
+    else:
+       logging.info('------ NOT MY BUSINESS -------')
 def init_root_path(zk):
     print '>>>>',ROOT_PATH, zk.exists(ROOT_PATH)
     if not zk.exists(ROOT_PATH):
        
        zk.create(ROOT_PATH)
+def get_my_id(id):
+    return int(str(id).split('/')[2])
 
 if __name__ == "__main__":
    print sys.argv
@@ -44,9 +51,9 @@ if __name__ == "__main__":
    init_root_path(zk)
 
 
-   me=zk.create(ROOT_PATH+'/',sequence=True, ephemeral=True)
+   my_id=get_my_id(zk.create(ROOT_PATH+'/',sequence=True, ephemeral=True))
    zk.ChildrenWatch(ROOT_PATH)(my_watch_chilren)
 
-   logging.info('>>>>> i am :%s' %me)
+   logging.info('>>>>> i am :%s' %my_id)
    while True:
        pass
